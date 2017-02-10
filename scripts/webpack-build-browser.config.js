6608b1d114ef9c04c919ce2ff4d5f8e42f324778
Chris Decoster <impaler777@gmail.com>
2017-02-10 16:32:25 +1100

    fix builds and npm run scripts.

--- scripts/webpack-build-browser.config.js
+++ scripts/webpack-build-browser.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
+const DEVELOPMENT = process.env.DEV
 
-module.exports = {
-    entry: path.join(__dirname, '../../src/promise-sequences.js'),
+let webpackConfig = {
+    entry: path.join(__dirname, '../src/promise-sequences.js'),
     output: {
-        path: path.join(__dirname, '../../build'),
+        path: path.join(__dirname, '../build'),
         filename: 'promise-sequences.umd.min.js',
         library: 'sequences',
         libraryTarget: 'umd'
@@ -17,11 +18,17 @@ module.exports = {
             }
         ],
     },
-    devtool: 'source-map',
     plugins: [
-        new webpack.IgnorePlugin(/\.(css|less)$/),
         new webpack.optimize.UglifyJsPlugin({
             minimize: true
         })
     ],
 }
+
+if (DEVELOPMENT) {
+  webpackConfig = Object.assign(webpackConfig, {
+    devtool: 'source-map',
+  })
+}
+
+module.exports = webpackConfig
--- scripts/webpack-build-node.config.js
+++ scripts/webpack-build-node.config.js
@@ -1,12 +1,13 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
+const DEVELOPMENT = process.env.DEV
 
-module.exports = {
-    entry: path.join(__dirname, '../../src/promise-sequences.js'),
+let webpackConfig = {
+    entry: path.join(__dirname, '../src/promise-sequences.js'),
     target: 'node',
     output: {
-        path: path.join(__dirname, '../../build'),
-        filename: 'promise-sequences.commonjs.js',
+        path: path.join(__dirname, '../build'),
+        filename: 'promise-sequences.js',
         libraryTarget: 'commonjs'
     },
     module: {
@@ -16,11 +17,17 @@ module.exports = {
                 loader: 'babel-loader'
             },
         ],
-    },
+    }
+}
+
+if (DEVELOPMENT) {
+  webpackConfig = Object.assign(webpackConfig, {
     devtool: 'source-map',
     plugins: [
-        new webpack.IgnorePlugin(/\.(css|less)$/),
-        new webpack.BannerPlugin('require("source-map-support").install();',
-            {raw: true, entryOnly: false}),
-    ],
+      new webpack.BannerPlugin('require("source-map-support").install();',
+        {raw: true, entryOnly: false}),
+    ]
+  })
 }
+
+module.exports = webpackConfig
